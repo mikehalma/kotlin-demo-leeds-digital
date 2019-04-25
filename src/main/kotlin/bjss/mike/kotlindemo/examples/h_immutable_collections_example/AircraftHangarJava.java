a96fa128e65adc9ad7c47beabf4e37215b6bbfdd
Mike Halma <mike_halma@yahoo.co.uk>
2019-04-25 09:40:59 +0100

    Tidy up imports

--- src/main/kotlin/bjss/mike/kotlindemo/examples/h_immutable_collections_example/AircraftHangarJava.java
+++ src/main/kotlin/bjss/mike/kotlindemo/examples/h_immutable_collections_example/AircraftHangarJava.java
@@ -1,6 +1,5 @@
 package bjss.mike.kotlindemo.examples.h_immutable_collections_example;
 
-import bjss.mike.kotlindemo.examples.a_data_class_example.EngineJava;
 import bjss.mike.kotlindemo.examples.b_null_safety_example.AircraftJava;
 
 import java.util.List;
